5773e1f0691de99a948e6e7f037509d934af900b
mochi <mochimou@icloud.com>
2022-02-03 07:13:25 +0000

    output static generated equivalent pairs

--- cmd/updatestatic/main.go
+++ cmd/updatestatic/main.go
@@ -1,24 +1,31 @@
 package main
 
+/*
+	Refresh the static.go file with the latest equivalent  pairs from an
+ 	EquivalentUnifiedIdeograph.txt file. Run this command from the root
+	of the repo:` `go run cmd/updatestatic/main.go``
+*/
+
 import (
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/mitranim/repr"
 
 	eqi "github.com/mochi-co/equivalent-unified-ideograph"
 )
 
 func init() {
 	index = flag.String("index", "data/EquivalentUnifiedIdeograph.txt", "the latest version of EquivalentUnifiedIdeograph.txt")
-	input = flag.String("input", "input.txt", "the input file containing the unicode characters")
-	output = flag.String("output", "output.txt", "the output file to write with the replaced characters")
+	output = flag.String("output", "static.go", "the static go file containing the equivalent pairs")
 	flag.Parse()
 }
 
 var (
 	index  *string
-	input  *string
 	output *string
 )
 
@@ -33,5 +40,15 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(pairs)
+	d := `package eqi 
+
+var Pairs = ` + repr.String(pairs)
+
+	d = strings.Replace(d, "eqi.", "", -1)
+	err = ioutil.WriteFile(*output, []byte(d), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Sucessfully updated %s with data from %s", *output, *index)
 }
